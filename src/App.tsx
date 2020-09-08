39aa12583a4c51edb5202116c0662a9cea08ab47
Panduru Ionut <panduru.ionut@hotmail.com>
2020-09-09 01:02:20 +0300

    fetch data from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Post from './Post';
 
@@ -17,7 +17,31 @@ function App() {
     caption: "Caption",
     imageUrl: "https://reactjs.org/logo-og.png"
   }
-  ]);
+  ])
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = () => {
+    fetch('http://127.0.0.1:8000/api/posts')
+      .then(
+        function (response) {
+          if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+              response.status);
+            return;
+          }
+          response.json().then(function (data) {
+            console.log(data);
+            setPosts(data);
+          });
+        }
+      )
+      .catch(function (err) {
+        console.log('Fetch Error :-S', err);
+      });
+  }
 
   return (
     <div className="app">
