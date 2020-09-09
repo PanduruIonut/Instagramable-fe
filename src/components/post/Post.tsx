e8381f2bec9777b4b90ff7cb14da8d79134187a8
Panduru Ionut <panduru.ionut@hotmail.com>
2020-09-09 09:33:38 +0300

    Add fallback for avatar and post picture

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -8,15 +8,16 @@ function Post({ username, commentsNr, likesNr,
     const [comments, setComments] = useState([]);
     const [comment, setComment] = useState('');
     const [tagValues, setTagValues] = useState(['']);
+    const [picture, setPicture] = useState('');
+    const [postPicture, setPostPicture] = useState('');
     const history = useHistory();
 
-
-    const clickMe = () => {
+    const onClick = () => {
         history.push({
             pathname: '/details',
             state: {
                 username, commentsNr, likesNr,
-                tags: tagValues, pictureUrl, avatarUrl, caption: tagValues,
+                tags: tagValues, pictureUrl: postPicture, avatarUrl: picture, caption: tagValues,
             }
         }
         );
@@ -26,14 +27,24 @@ function Post({ username, commentsNr, likesNr,
         setTagValues([...tagValues, caption]);
     }, [tags]);
 
+    React.useEffect(() => {
+        setPicture(pictureUrl);
+    }, [tags]);
+
+    const onPhotoError = () => {
+        setPicture("https://source.unsplash.com/random");
+        setPostPicture("https://source.unsplash.com/random");
+    }
+
+
     return (
-        <div className="post" onClick={(e) => clickMe()}>
+        <div className="post" onClick={() => onClick()}>
             <div className="post_header">
-                <Avatar className="post_avatar" alt="" src={avatarUrl}></Avatar>
+                <Avatar className="post_avatar" alt="" src={picture} onError={onPhotoError}></Avatar>
                 <h3>{username} </h3>
             </div>
 
-            <img className="post_image" src={pictureUrl} />
+            <img className="post_image" src={postPicture} onError={() => { onPhotoError() }} />
             <h4 className="post_text"><strong>{username}</strong> {caption}</h4>
             <form className="post_commentBox">
                 <input className="post_input" type="text" placeholder="Add a comment..."
