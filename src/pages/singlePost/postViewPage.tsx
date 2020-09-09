7eac469d2212b445fa417c74c80254d3b61931c0
Panduru Ionut <panduru.ionut@hotmail.com>
2020-09-09 09:30:50 +0300

    Convert SinglePostPage to functional component

--- src/pages/singlePost/postViewPage.tsx
+++ src/pages/singlePost/postViewPage.tsx
@@ -3,28 +3,25 @@ import PostViewComponent from '../../components/postView/postViewComponent';
 import './postViewPage.css';
 
 
-class SinglePostPage extends Component<any> {
-    public render() {
-
-        return (
-            <div className="app" >
-                <div className="app_header">
-                    <img className="app_headerImage"
-                        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-                        alt=""></img>
-                </div>
-                <div className="app_posts">
-                    <PostViewComponent username={this.props.location.state.username}
-                        commentsNr={this.props.location.state.commentsNr}
-                        likesNr={this.props.location.state.likesNr}
-                        tags={this.props.location.state.tags}
-                        pictureUrl={this.props.location.state.pictureUrl}
-                        avatarUrl={this.props.location.state.commentsNr.avatarUrl}
-                        caption={this.props.location.state.commentsNr.caption} ></PostViewComponent>
-                </div>
-            </div >
-        );
-    }
+const SinglePostPage = ({ location }) => {
+    return (
+        <div className="app" >
+            <div className="app_header">
+                <img className="app_headerImage"
+                    src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                    alt=""></img>
+            </div>
+            <div className="app_posts">
+                <PostViewComponent username={location.state.username}
+                    commentsNr={location.state.commentsNr}
+                    likesNr={location.state.likesNr}
+                    tags={location.state.tags}
+                    pictureUrl={location.state.pictureUrl}
+                    avatarUrl={location.state.commentsNr.avatarUrl}
+                    caption={location.state.commentsNr.caption} ></PostViewComponent>
+            </div>
+        </div >
+    );
 }
 
 export default SinglePostPage;
